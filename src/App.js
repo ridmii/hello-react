4cdf7e8b8576e38cd6cbb702117f30f6e7183a28
Ridmi Vancuylenburg <heyridmi@gmail.com>
2024-10-05 16:01:41 +0530

    Create User

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() { //Component
+  const [users, setUsers] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  function getUsers() {
+    axios.get("http://localhost:8080/users")
+    .then(function(response){
+      setUsers(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+
+  }
+
+  function handleUsername(event) {
+    setUsername(event.target.value);
+  }
+  function handlePassword(event) {
+    setPassword(event.target.value);
+  }
+  function handleEmail(event) {
+    setEmail(event.target.value);
+  }
+  function createUser(event) {
+    event.preventDefault();
+
+    const data = {
+      username: username,
+      password: password,
+      email: email,
+    }
+    axios.post("http://localhost:8080/users", data)
+    .then(function(response) { 
+      getUsers();
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.log(error);
+  })
 }
 
+  return (
+    <div className="App">
+      <button type="button" onClick={getUsers}>Get Users</button>
+      {users && users.map((row) => (
+<div key={row.id}>
+  {row.username} - {row.email}
+  </div>
+      ))
+    }
+    <h2>
+      Create User 
+    </h2>
+    <form onSubmit={createUser}>
+      <div>
+      <label>Username</label>
+      <input type="text" onChange={handleUsername} required/>
+      </div>
+
+<div>
+  <label>Password</label>
+  <input type="password" onChange={handlePassword} required/>
+</div>
+
+<div>
+  <label>Email</label>
+  <input type="email" onChange={handleEmail} required/>
+</div>
+
+<button type="submit">Create User</button>
+
+
+
+
+    </form>
+      </div>
+  );
+}
 export default App;
