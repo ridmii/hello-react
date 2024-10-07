d649ede2f3d9fd9ea4375687e814197b5d149c1a
Ridmi Vancuylenburg <heyridmi@gmail.com>
2024-10-07 17:58:29 +0530

    orders

--- src/Products.js
+++ src/Products.js
@@ -2,49 +2,48 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function Product() {
-    const [products, setProducts] = useState([]); // Initialize as empty array
-    const [categories, setCategories] = useState([]);
+
+    const [products, setProducts] = useState(null);
+    const [categories, setCategories] = useState(null);
 
     useEffect(() => {
-        // Fetch products
         axios.get("http://localhost:8080/products")
-        .then(function (response) {
-            setProducts(response.data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+            .then(function (response) {
+                setProducts(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
 
-        // Fetch categories
         axios.get("http://localhost:8080/categories")
-        .then(function (response) {
-            setCategories(response.data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
-    }, []);
+            .then(function (response) {
+                setCategories(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }, [])
 
     const [name, setName] = useState("");
     const [price, setPrice] = useState("");
     const [qty, setQty] = useState("");
-    const [categoryId, setCategoryId] = useState("");
+    const [categoryId, setCategoryId] = useState(null);
+
 
     function handleName(event) {
         setName(event.target.value);
     }
 
     function handlePrice(event) {
-        setPrice(event.target.value);
+        setPrice(parseFloat(event.target.value));
     }
 
     function handleQuantity(event) {
-        setQty(event.target.value);
+        setQty(parseInt(event.target.value));
     }
 
     function handleCategory(event) {
-        setCategoryId(event.target.value); // Set the selected category ID
+        setCategoryId(event.target.value);
     }
 
     function createProduct(event) {
@@ -67,58 +66,141 @@ function Product() {
             });
     }
 
+    const [edit, setEdit] = useState(false);
+    const [productId, setProductId] = useState(null);
+
+    function getProducts() {
+        axios.get("http://localhost:8080/products")
+            .then(function (response) {
+                setProducts(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    function updateProduct(event) {
+        event.preventDefault();
+
+        const data = {
+            name: name,
+            price: price,
+            quantity: qty,
+            categoryId: categoryId
+        }
+
+        axios.put("http://localhost:8080/products/" + productId, data)
+            .then(function (response) {
+                getProducts();
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
     return (
         <div>
             <h1>Products</h1>
 
-            {products.map((product) => (
-                <div key={product.id}>
-                    <h2>{product.name}</h2>
-                    <p>Category: {product.category?.name}</p>
-                    <p>Price: {product.price}</p>
-                    <p>Qty: {product.quantity}</p>
-                    <p>{product.price}</p>
-                </div>
-            ))}
+            {products && products.map((product) => {
+                return (
+                    <div key={product.id}>
+                        <h2>{product.name}</h2>
+                        <p>Category: {product.category?.name}</p>
+                        <p>Price: {product.price}</p>
+                        <p>Qty: {product.quantity}</p>
+                        <button type="button" className="btn btn-primary" onClick={() => {
+                            setEdit(true);
+                            setProductId(product.id);
+                            setName(product.name);
+                            setPrice(product.price);
+                            setQty(product.quantity);
+                            setCategoryId(product.category?.id);
+                        }}>Edit</button>
+                    </div>
+                )
+            })
+            }
 
-            <form onSubmit={createProduct}>
-                <div>
-                    <label>Name</label>
-                    <input type="text" onChange={handleName} required />
-                </div>
+            {!edit &&
 
-                <br />
+                <form onSubmit={createProduct}>
+                    <div>
+                        <label>Name</label>
+                        <input type="text" onChange={handleName} required />
+                    </div>
 
-                <div>
-                    <label>Price</label>
-                    <input type="text" onChange={handlePrice} required />
-                </div>
+                    <br />
 
-                <br />
+                    <div>
+                        <label>Price</label>
+                        <input type="text" onChange={handlePrice} required />
+                    </div>
 
-                <div>
-                    <label>Quantity</label>
-                    <input type="text" onChange={handleQuantity} required />
-                </div>
+                    <br />
 
-                <div>
-                    <label>Category</label>
-                    <select value={categoryId} onChange={handleCategory} required>
-                        <option value="">Select a category</option>
-                        {categories.map((category) => (
-                            <option key={category.id} value={category.id}>
-                                {category.name}
-                            </option>
-                        ))}
-                    </select>
-                </div>
+                    <div>
+                        <label>Quantity</label>
+                        <input type="text" onChange={handleQuantity} required />
+                    </div>
 
-                <br />
-                <br />
-                <button type="submit">Create Product</button>
-            </form>
+                    <div>
+                        <label>Category</label>
+                        <select onChange={handleCategory} value={categoryId || ""} required>
+    <option value="">Select a category</option>
+    {categories && categories.map((category) => (
+        <option key={category.id} value={category.id}>{category.name}</option>
+    ))}
+</select>
+
+                    </div>
+
+                    <br />
+                    <br />
+                    <button type="submit">Create Product</button>
+                </form>
+            }
+
+            {edit &&
+                <form onSubmit={updateProduct}>
+                    <div>
+                        <label>Name</label>
+                        <input type="text" onChange={handleName} value={name} required />
+                    </div>
+
+                    <br />
+
+                    <div>
+                        <label>Price</label>
+                        <input type="text" onChange={handlePrice} value={price} required />
+                    </div>
+
+                    <br />
+
+                    <div>
+                        <label>Quantity</label>
+                        <input type="text" onChange={handleQuantity} value={qty} required />
+                    </div>
+
+                    <div>
+                        <label>Category</label>
+                        <select onChange={handleCategory} value={categoryId || ""} required>
+    <option value="">Select a category</option>
+    {categories && categories.map((category) => (
+        <option key={category.id} value={category.id}>{category.name}</option>
+    ))}
+</select>
+
+                    </div>
+
+                    <br />
+                    <br />
+                    <button type="submit">Update Product</button>
+                </form>
+            }
+
         </div>
-    );
+    )
 }
 
 export default Product;
